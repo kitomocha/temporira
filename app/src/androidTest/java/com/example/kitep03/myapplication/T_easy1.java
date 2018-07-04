1778618de77b25dae315978de343fe25b70ce848
Ryousuke Aruga <b1606254@planet.kanazawa-it.ac.jp>
2018-07-04 17:22:51 +0900

    基本用

---
+++ app/src/androidTest/java/com/example/kitep03/myapplication/T_easy1.java
@@ -0,0 +1,12 @@
+package com.example.kitep03.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class T_easy1 extends AppCompatActivity {
+    protected  void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
