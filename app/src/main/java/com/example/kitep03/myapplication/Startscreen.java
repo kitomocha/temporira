a1fd3faabf5549265cc4fad269e99729e0556841
mori <b1633744@planet.kanazawa-it.ac.jp>
2018-10-04 18:45:28 +0900

    スタート画面

---
+++ app/src/main/java/com/example/kitep03/myapplication/Startscreen.java
@@ -0,0 +1,13 @@
+package com.example.kitep03.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Startscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_startscreen);
+    }
+}
